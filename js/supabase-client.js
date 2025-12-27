ef6d441ac4b0f1d91b7c2177ac73ef13cd17efbc
lenrong <hendra.keumamuju@gmail.com>
2025-12-27 15:58:29 +0800

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,1 +1,11 @@
+// js/supabase-client.js
+const SUPABASE_URL = 'https://dacurshwofmzaogeutnm.supabase.co'; // 🔁 GANTI dengan Project URL Anda
+const SUPABASE_ANON_KEY = 'sb_publishable_yQnHA8sybmzfMNwsJrpZ6g_I11EObKR'; // 🔁 GANTI dengan Publishable Key Anda
 
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true
+  }
+});
